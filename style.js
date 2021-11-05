ae2af5516b1b52601c07a1d466dfad2d89b8249d
MathieuWasselin <wasselinmathieu@gmail.com>
2021-11-05 09:26:20 +0100

    changement

--- style.js
+++ style.js
@@ -1,3 +1,10 @@
+
+document.getElementById("validate").onclick = function(){
+    document.getElementById("hello").innerHTML="Bonjour "+id1.value;
+    }
+
+
+    
 var valeur=prompt("ecrire votre prenom");
 document.writeln("Bonjour"+ valeur);
 
